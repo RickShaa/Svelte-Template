b0099be091fe23f1d9bbfec1d7ebd2170d52cd71
RickShaa <r.shaffer@gmx.de>
2022-06-27 13:02:53 +0200

    first

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	let count = 0;
+</script>
+
+<main>
+	<div class="flex flex-col items-center">
+		<div class="text-slate-600 text-9xl">{count}</div>
+		<button class="text-slate-600 text-9xl bg-slate-300 p-3 rounded-lg" on:click={() => ++count}
+			>Add</button
+		>
+	</div>
+</main>
